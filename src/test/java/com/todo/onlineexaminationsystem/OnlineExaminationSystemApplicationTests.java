d7628c8d2180a8461ee6ef2b8cb4e8abd4b04438
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-28 17:00:06 +0530

    Initial commit

---
+++ src/test/java/com/todo/onlineexaminationsystem/OnlineExaminationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.onlineexaminationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExaminationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
