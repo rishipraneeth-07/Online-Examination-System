235d04c68a7272e75adae21312cc90ea6abee167
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-01 18:43:32 +0530

    Added role class

--- src/main/java/com/todo/onlineexaminationsystem/user/role.java
+++ src/main/java/com/todo/onlineexaminationsystem/user/role.java
@@ -1,4 +1,21 @@
 package com.todo.onlineexaminationsystem.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+
+
 }
