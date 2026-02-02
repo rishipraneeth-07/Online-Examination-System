0944036f5dd92cb39a93ecba3b109b8f420f8b61
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-02 22:54:05 +0530

    Updated service impl

--- src/main/java/com/todo/onlineexaminationsystem/user/UserServiceImpl.java
+++ src/main/java/com/todo/onlineexaminationsystem/user/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package com.todo.onlineexaminationsystem.user;
 
-public class UserServiceImpl {
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public User createUser(User user) {
+        if (userRepo.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("User with this email already exists");
+        }
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return userRepo.findByEmail(email);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepo.existsByEmail(email);
+    }
 }
