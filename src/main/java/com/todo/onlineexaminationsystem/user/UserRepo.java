35303b5ca59a7182e7cbc1b9019e279806f108d4
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-02 22:42:36 +0530

    Added User Repo

--- src/main/java/com/todo/onlineexaminationsystem/user/UserRepo.java
+++ src/main/java/com/todo/onlineexaminationsystem/user/UserRepo.java
@@ -1,4 +1,13 @@
 package com.todo.onlineexaminationsystem.user;
 
-public class UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
 }
