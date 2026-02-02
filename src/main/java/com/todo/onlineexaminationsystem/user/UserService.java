7fdbe11c64da2a28c71d58ad50ebf21cb4829743
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-02 22:49:56 +0530

    Updated Service interface

--- src/main/java/com/todo/onlineexaminationsystem/user/UserService.java
+++ src/main/java/com/todo/onlineexaminationsystem/user/UserService.java
@@ -1,4 +1,11 @@
 package com.todo.onlineexaminationsystem.user;
 
+import java.util.Optional;
+
 public interface UserService {
+    User createUser(User user);
+
+    Optional<User> getUserByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
