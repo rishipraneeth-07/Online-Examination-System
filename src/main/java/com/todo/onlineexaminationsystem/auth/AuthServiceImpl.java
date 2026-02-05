8cdceb959f9f395697bc59bcc182e4a7c165b803
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-05 19:04:28 +0530

    Updated AuthServiceImpl

--- src/main/java/com/todo/onlineexaminationsystem/auth/AuthServiceImpl.java
+++ src/main/java/com/todo/onlineexaminationsystem/auth/AuthServiceImpl.java
@@ -1,4 +1,33 @@
 package com.todo.onlineexaminationsystem.auth;
 
-public class AuthServiceImpl {
+import com.todo.onlineexaminationsystem.dto.LoginRequest;
+import com.todo.onlineexaminationsystem.dto.LoginResponse;
+import com.todo.onlineexaminationsystem.user.User;
+import com.todo.onlineexaminationsystem.user.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    private UserService userService;
+    public AuthServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest loginRequest) {
+        User user = userService.getUserByEmail(loginRequest.getEmail())
+                .orElseThrow(() -> new RuntimeException("Invalid email or password"));
+        if (!user.isEnabled()) {
+            throw new RuntimeException("User account is disabled");
+        }
+        if (!user.getPassword().equals(loginRequest.getPassword())) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        return new LoginResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getRole().getName()
+        );
+    }
 }
