2d588e10a94e088116aafd63ab3e44f5ee632031
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-06 22:19:52 +0530

    Updated Auth Controller

--- src/main/java/com/todo/onlineexaminationsystem/auth/AuthController.java
+++ src/main/java/com/todo/onlineexaminationsystem/auth/AuthController.java
@@ -1,4 +1,25 @@
 package com.todo.onlineexaminationsystem.auth;
 
-public interface AuthController {
+import com.todo.onlineexaminationsystem.dto.LoginRequest;
+import com.todo.onlineexaminationsystem.dto.LoginResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        LoginResponse response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
 }
