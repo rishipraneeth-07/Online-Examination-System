65c7d6f70586b859cec3245814f79bd85eb9cadb
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-07 11:22:00 +0530

    secure password storage and login verification

--- src/main/java/com/todo/onlineexaminationsystem/auth/AuthServiceImpl.java
+++ src/main/java/com/todo/onlineexaminationsystem/auth/AuthServiceImpl.java
@@ -4,13 +4,16 @@ import com.todo.onlineexaminationsystem.dto.LoginRequest;
 import com.todo.onlineexaminationsystem.dto.LoginResponse;
 import com.todo.onlineexaminationsystem.user.User;
 import com.todo.onlineexaminationsystem.user.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
     private UserService userService;
-    public AuthServiceImpl(UserService userService) {
+    private final PasswordEncoder passwordEncoder;
+    public AuthServiceImpl(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -20,7 +23,9 @@ public class AuthServiceImpl implements AuthService {
         if (!user.isEnabled()) {
             throw new RuntimeException("User account is disabled");
         }
-        if (!user.getPassword().equals(loginRequest.getPassword())) {
+        if (!passwordEncoder.matches(
+                loginRequest.getPassword(),
+                user.getPassword())){
             throw new RuntimeException("Invalid email or password");
         }
 
