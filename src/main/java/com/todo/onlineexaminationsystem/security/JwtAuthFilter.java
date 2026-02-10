9786cd20d4e8b7689b07d33a8ae7007b819a4a5c
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-10 17:44:13 +0530

    Added @Component in AuthFIlter

--- src/main/java/com/todo/onlineexaminationsystem/security/JwtAuthFilter.java
+++ src/main/java/com/todo/onlineexaminationsystem/security/JwtAuthFilter.java
@@ -7,11 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
-
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
 
