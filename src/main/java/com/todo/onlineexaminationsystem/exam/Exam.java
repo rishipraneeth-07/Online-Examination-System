6bc5c3c64e7ccd84aa141a907cfb1380e56b9215
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-11 12:30:44 +0530

    Added exam entity

---
+++ src/main/java/com/todo/onlineexaminationsystem/exam/Exam.java
@@ -0,0 +1,41 @@
+package com.todo.onlineexaminationsystem.exam;
+
+import com.todo.onlineexaminationsystem.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "exams")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Exam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private User createdBy;
+}
