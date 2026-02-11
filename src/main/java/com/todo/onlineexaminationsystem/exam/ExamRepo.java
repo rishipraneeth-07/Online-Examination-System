7d54d9e0868934636d64b4c09f5d5a7721f6f6bd
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-11 12:34:32 +0530

    Added Exam Repo with custom queries

---
+++ src/main/java/com/todo/onlineexaminationsystem/exam/ExamRepo.java
@@ -0,0 +1,15 @@
+package com.todo.onlineexaminationsystem.exam;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ExamRepo extends JpaRepository<Exam, Long> {
+    List<Exam> findByCreatedBy(User teacher);
+
+    List<Exam> findByStartTimeBeforeAndEndTimeAfter(LocalDateTime now1, LocalDateTime now2);
+}
