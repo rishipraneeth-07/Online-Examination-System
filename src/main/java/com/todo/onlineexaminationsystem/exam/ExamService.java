7e97c745db333e63dea0085ef384d61ba7182802
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-11 12:38:22 +0530

    Updated methods in service

--- src/main/java/com/todo/onlineexaminationsystem/exam/ExamService.java
+++ src/main/java/com/todo/onlineexaminationsystem/exam/ExamService.java
@@ -1,4 +1,14 @@
 package com.todo.onlineexaminationsystem.exam;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ExamService {
+    Exam createExam(Exam exam, String teacherEmail);
+
+    Exam getExamById(Long examId);
+
+    List<Exam> getExamsByTeacher(String teacherEmail);
+
+    List<Exam> getActiveExams(LocalDateTime now);
 }
