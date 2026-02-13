0734c19119d023b94b220edff17f21262fc0cf79
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-13 22:45:12 +0530

    Updated Exam repo

--- src/main/java/com/todo/onlineexaminationsystem/question/QuestionRepo.java
+++ src/main/java/com/todo/onlineexaminationsystem/question/QuestionRepo.java
@@ -1,4 +1,12 @@
 package com.todo.onlineexaminationsystem.question;
 
-public class QuestionRepo {
+import com.todo.onlineexaminationsystem.exam.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepo extends JpaRepository<Question, Integer> {
+    List<Question> findByExam(Exam exam);
 }
