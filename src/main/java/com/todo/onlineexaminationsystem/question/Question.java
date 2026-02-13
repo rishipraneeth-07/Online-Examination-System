0f0a0b13414af43f10009b272be554bec495f217
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-13 22:40:40 +0530

    Updated exam entity

--- src/main/java/com/todo/onlineexaminationsystem/question/Question.java
+++ src/main/java/com/todo/onlineexaminationsystem/question/Question.java
@@ -1,4 +1,40 @@
 package com.todo.onlineexaminationsystem.question;
 
+import com.todo.onlineexaminationsystem.exam.Exam;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String questionText;
+
+    @Column(nullable = false)
+    private String optionA;
+
+    @Column(nullable = false)
+    private String optionB;
+
+    @Column(nullable = false)
+    private String optionC;
+
+    @Column(nullable = false)
+    private String optionD;
+
+    @Column(nullable = false)
+    private String correctAnswer; // A, B, C, D
+
+    @ManyToOne
+    @JoinColumn(name = "exam_id", nullable = false)
+    private Exam exam;
 }
