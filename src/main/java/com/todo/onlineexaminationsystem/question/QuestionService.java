9694865080af7a12082715d9044eaa439b6c3bd2
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-14 11:31:41 +0530

    Added Methods in Service

--- src/main/java/com/todo/onlineexaminationsystem/question/QuestionService.java
+++ src/main/java/com/todo/onlineexaminationsystem/question/QuestionService.java
@@ -1,4 +1,9 @@
 package com.todo.onlineexaminationsystem.question;
 
+import java.util.List;
+
 public interface QuestionService {
+    Question addQuestion(Long examId, Question question, String teacherEmail);
+
+    List<Question> getQuestionsByExam(Long examId);
 }
