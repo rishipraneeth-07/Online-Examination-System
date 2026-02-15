6bdb8be32311e0233188c90715ea95f69783ed09
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-15 10:07:47 +0530

    Updated Question Controller

--- src/main/java/com/todo/onlineexaminationsystem/question/QuestionController.java
+++ src/main/java/com/todo/onlineexaminationsystem/question/QuestionController.java
@@ -1,4 +1,39 @@
 package com.todo.onlineexaminationsystem.question;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class QuestionController {
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping("/teacher/exams/{examId}/questions")
+    public ResponseEntity<Question> addQuestion(@PathVariable Long examId,
+                                                @RequestBody Question question,
+                                                Authentication authentication) {
+
+        String teacherEmail = authentication.getName();
+
+        Question savedQuestion =
+                questionService.addQuestion(examId, question, teacherEmail);
+
+        return ResponseEntity.ok(savedQuestion);
+    }
+
+    @GetMapping("/exams/{examId}/questions")
+    public ResponseEntity<List<Question>> getQuestionsByExam(@PathVariable Long examId) {
+
+        List<Question> questions =
+                questionService.getQuestionsByExam(examId);
+
+        return ResponseEntity.ok(questions);
+    }
 }
