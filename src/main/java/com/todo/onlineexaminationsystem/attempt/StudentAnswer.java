04024d594077c55c8ac5c70b5d903e21582b86f1
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-17 00:34:58 +0530

    Updated StudentAnswer entity

--- src/main/java/com/todo/onlineexaminationsystem/attempt/StudentAnswer.java
+++ src/main/java/com/todo/onlineexaminationsystem/attempt/StudentAnswer.java
@@ -1,4 +1,31 @@
 package com.todo.onlineexaminationsystem.attempt;
 
+import com.todo.onlineexaminationsystem.question.Question;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "student_answers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "attempt_id", nullable = false)
+    private ExamAttempt examAttempt;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(nullable = false)
+    private String selectedAnswer;
 }
