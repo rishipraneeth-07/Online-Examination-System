1e9e2c64007a49ac4beda5204cfb2b07cbe8f88c
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-16 18:56:45 +0530

    Updated ExamAttempt entity

--- src/main/java/com/todo/onlineexaminationsystem/attempt/ExamAttempt.java
+++ src/main/java/com/todo/onlineexaminationsystem/attempt/ExamAttempt.java
@@ -1,4 +1,37 @@
 package com.todo.onlineexaminationsystem.attempt;
 
+import com.todo.onlineexaminationsystem.exam.Exam;
+import com.todo.onlineexaminationsystem.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "exam_attempts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExamAttempt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private User student;
+
+    @ManyToOne
+    @JoinColumn(name = "exam_id", nullable = false)
+    private Exam exam;
+
+    @Column(nullable = false)
+    private Integer score;
+
+    @Column(nullable = false)
+    private LocalDateTime submittedAt;
 }
