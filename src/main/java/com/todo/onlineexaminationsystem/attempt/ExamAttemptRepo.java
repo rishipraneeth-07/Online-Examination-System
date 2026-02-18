1133daa9613a41d54d22eda040b4df9e40eb43a9
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-18 09:25:33 +0530

    Updated Repo and added Custom methos

--- src/main/java/com/todo/onlineexaminationsystem/attempt/ExamAttemptRepo.java
+++ src/main/java/com/todo/onlineexaminationsystem/attempt/ExamAttemptRepo.java
@@ -1,4 +1,18 @@
 package com.todo.onlineexaminationsystem.attempt;
 
-public interface ExamAttemptRepo {
+import com.todo.onlineexaminationsystem.exam.Exam;
+import com.todo.onlineexaminationsystem.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExamAttemptRepo extends JpaRepository<ExamAttempt,Long> {
+    List<ExamAttempt> findByStudent(User student);
+
+    List<ExamAttempt> findByExam(Exam exam);
+
+    boolean existsByStudentAndExam(User student, Exam exam);
 }
