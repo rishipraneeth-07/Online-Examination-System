1e23fe4d0448b0b68b0cde1c00e564a3cef1e846
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-19 22:13:38 +0530

    Updated StudentAnswer Repo

--- src/main/java/com/todo/onlineexaminationsystem/attempt/StudentAnswerRepo.java
+++ src/main/java/com/todo/onlineexaminationsystem/attempt/StudentAnswerRepo.java
@@ -1,4 +1,11 @@
 package com.todo.onlineexaminationsystem.attempt;
 
-public class StudentAnswerRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentAnswerRepo extends JpaRepository<StudentAnswer,Long> {
+    List<StudentAnswer> findByExamAttempt(ExamAttempt examAttempt);
 }
