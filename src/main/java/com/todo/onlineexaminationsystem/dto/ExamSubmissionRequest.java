3300f2dff0132d9caa810c6c92f99a49ae48bc8b
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-20 09:44:03 +0530

    Updated ExamSubmissionRequest dto

--- src/main/java/com/todo/onlineexaminationsystem/dto/ExamSubmissionRequest.java
+++ src/main/java/com/todo/onlineexaminationsystem/dto/ExamSubmissionRequest.java
@@ -1,4 +1,15 @@
 package com.todo.onlineexaminationsystem.dto;
 
+import lombok.*;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExamSubmissionRequest {
+
+    private Long examId;
+    private Map<Long, String> answers;
 }
