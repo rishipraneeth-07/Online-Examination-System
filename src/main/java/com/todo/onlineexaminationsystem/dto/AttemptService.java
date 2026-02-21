4e79b286190d9ae342af46cb6e5d010163ea4ed1
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-21 10:30:08 +0530

    Updated Attempt Service interface

--- src/main/java/com/todo/onlineexaminationsystem/dto/AttemptService.java
+++ src/main/java/com/todo/onlineexaminationsystem/dto/AttemptService.java
@@ -1,4 +1,7 @@
 package com.todo.onlineexaminationsystem.dto;
 
+import com.todo.onlineexaminationsystem.attempt.ExamAttempt;
+
 public interface AttemptService {
+    ExamAttempt submitExam(ExamSubmissionRequest request, String studentEmail);
 }
