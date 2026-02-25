2505283b493a78b7066c6359240d863f06c299cd
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-25 18:56:59 +0530

    Updated Resource not found class

--- src/main/java/com/todo/onlineexaminationsystem/exception/ResourceNotFoundException.java
+++ src/main/java/com/todo/onlineexaminationsystem/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.todo.onlineexaminationsystem.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends ApiException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
