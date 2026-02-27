dd94aad52a3670eed44721708bff066e5a102735
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-02-27 22:20:16 +0530

    Updated Error response

--- src/main/java/com/todo/onlineexaminationsystem/exception/ErrorResponse.java
+++ src/main/java/com/todo/onlineexaminationsystem/exception/ErrorResponse.java
@@ -1,4 +1,14 @@
 package com.todo.onlineexaminationsystem.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class ErrorResponse {
+    private String message;
+    private int status;
+    private LocalDateTime timestamp;
 }
