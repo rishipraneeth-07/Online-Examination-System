65c73ac98f25b3b89633b6978eeaa4bd4f1cf828
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-03-05 21:05:19 +0530

    added @Configuration

--- src/main/java/com/todo/onlineexaminationsystem/Config/DataInitializer.java
+++ src/main/java/com/todo/onlineexaminationsystem/Config/DataInitializer.java
@@ -6,8 +6,10 @@ import com.todo.onlineexaminationsystem.user.User;
 import com.todo.onlineexaminationsystem.user.UserRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class DataInitializer {
     @Bean
     CommandLineRunner initData(
