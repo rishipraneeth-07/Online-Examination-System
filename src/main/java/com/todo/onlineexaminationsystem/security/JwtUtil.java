d99994776776bc9c0f249dbdb6ecdcf91cec2266
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-03-06 09:32:23 +0530

    fixed secret key

--- src/main/java/com/todo/onlineexaminationsystem/security/JwtUtil.java
+++ src/main/java/com/todo/onlineexaminationsystem/security/JwtUtil.java
@@ -3,26 +3,31 @@ package com.todo.onlineexaminationsystem.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final String SECRET_KEY = "my-secret-key";
+    private static final String SECRET_KEY = "myverysecuresecretkeymyverysecuresecretkey123456";
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
     public String generateToken(String email, String role) {
         return Jwts.builder()
                 .setSubject(email)
                 .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
     public Claims extractClaims(String token) {
+
         return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+                .setSigningKey(key)
                 .parseClaimsJws(token)
                 .getBody();
     }
